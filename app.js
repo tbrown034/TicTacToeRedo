7fbd37c15bcb79d1b24aec81c7b6076356022825
tbrown034 <tbrown034@gmail.com>
2022-09-02 22:53:29 -0500

    adding xs and os working on board but not functional beyond that

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const state = {};
 const resetState = () => {
     state.board = ["", "", "", "", "", "", "", "", ""]
+    state.currentPlayer = "x"
 }
 
 // Dom Selector ----> grabbing elements
@@ -9,20 +10,33 @@ const boardElement = document.getElementById('board');
 
 //Dom Manipulators ----> rendering
 const renderBoard = () => {
+      boardElement.innerHTML = '';
     for (let i = 0; i < state.board.length; i++) {
         const square = state.board[i];
         const cellElement = document.createElement('div');
         cellElement.classList.add('cell')
         cellElement.innerHTML = square;
-        boardElement.appendChild(cellElement)
+        cellElement.dataset.index = i;
+        boardElement.appendChild(cellElement);
 
     }
-
 }
 
 
-
+// EventListeners
+boardElement.addEventListener('click', (event) => {
+    if (event.target.className !== 'cell') return;
+    let cellIndex = event.target.dataset.index;
+    state.board[cellIndex] = state.currentPlayer;
+    if(state.currentPlayer === 'x') {
+    state.currentPlayer = 'o'}
+    else {state.currentPlayer = 'x'}
+    // statusDisplay.innerHTML = currentPlayerTurn();
+    renderBoard()
+  }
+)
 
 // Bootstrapping ----> Need to call(invoke) formulas
-resetState()
-renderBoard()
+resetState();
+renderBoard();
+
